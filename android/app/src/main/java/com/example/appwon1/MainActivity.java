d01100c3b826dad8ff0cfb48571155eb0cdfa82b
Jagadeeshkugan <jagadeeshkugan2702@gmail.com>
2023-02-01 19:04:46 +0530

    message

--- android/app/src/main/java/com/example/appwon1/MainActivity.java
+++ android/app/src/main/java/com/example/appwon1/MainActivity.java
@@ -8,11 +8,6 @@ import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 
-    int act[]={R.drawable.img2, R.drawable.img3,R.drawable.img4};
-
-    viewFlipper =(ViewFlipper)findViewbyid(R.id.viewFlipper);
-    public VOID main(){
-        SYSTEM.out.println();
-    }
+   
      
 }
